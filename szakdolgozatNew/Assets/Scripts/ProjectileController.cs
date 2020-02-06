b45dcdfa63dc9c671bac6aaf92f47196a7db03d3
8dave8 <43006387+8dave8@users.noreply.github.com>
2020-02-06 17:10:09 +0100

    llamaEnemy

---
+++ szakdolgozatNew/Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Player" || col.gameObject.layer == 8){
+            Destroy(gameObject);
+            Debug.Log("ded");
+        }
+    }
+}
---
+++ szakdolgozatNew/Assets/Scripts/ProjectileSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSpawner : MonoBehaviour
+{
+    public bool FacingLeft;
+    public float speed;
+    public GameObject Projectile; 
+    void Start()
+    {
+        if(FacingLeft) speed *= -1;
+        StartCoroutine("Spawn");
+    }
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(2f);
+        var CurrentObstacle = Instantiate(Projectile,new Vector3(transform.position.x-0.2f,transform.position.y+1.1f,0f),Quaternion.identity);
+        CurrentObstacle.GetComponent<Rigidbody2D>().AddForce(new Vector2(speed,0f),ForceMode2D.Impulse);
+        StartCoroutine("Spawn");
+    }
+}
