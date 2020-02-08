4df58c8140aff028dda10c0ea19d7d4416207e55
8dave8 <43006387+8dave8@users.noreply.github.com>
2020-02-08 08:08:18 +0100

    menuanim

---
+++ szakdolgozatNew/Assets/Scripts/MenuAnimator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAnimator : MonoBehaviour
+{
+    public Animator Menu;
+    public void MenuToSettings()
+    {
+        
+    }
+    public void SettingsToMenu()
+    {
+
+    }
+    public void MenuToStats()
+    {
+
+    }
+    public void StatsToMenu()
+    {
+
+    }
+    public void MenuToLevels()
+    {
+
+    }
+    public void LevelsToMenu()
+    {
+        
+    }
+}
